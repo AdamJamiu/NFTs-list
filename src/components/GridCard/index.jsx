492c2fda83ae24175cc1ab6db852815a8367ac45
AdamJamiu <adamjamiu98@gmail.com>
2023-03-10 16:21:09 +0100

    grid card updates and responsiveness

--- src/components/GridCard/index.jsx
+++ src/components/GridCard/index.jsx
@@ -9,6 +9,9 @@ import AppContext from '../../context/appContext';
 export const GridCard = () => {
     const { data } = fetchDetails();
     const { handleOpen, setId } = React.useContext(AppContext);
+    const convertDate = (props) => {
+        return new Date(props).toLocaleDateString('default', { day: "2-digit", month: 'long' });
+    }
 
     return (
         <>
@@ -18,8 +21,11 @@ export const GridCard = () => {
                         <div className='img-wrapper' onClick={() => { handleOpen(); setId(item?.token_id) }}>
                             <img src={item?.cached_file_url} alt="Card Image" />
                         </div>
-                        <p>{item?.token_id}</p>
-                        <h2>{item?.chain}</h2>
+                        <div className='card_cont'>
+                            <p>{item?.token_id}</p>
+                            <h2>{item?.chain}</h2>
+                            <span className='card-info'>Update date: <span>{convertDate(item?.updated_date)}</span></span>
+                        </div>
                         <a href={`https://opensea.io/assets/ethereum/${item?.contract_address}/${item?.token_id}`}>
                             <button className='card-btn'>
                                 <BoltIcon />
